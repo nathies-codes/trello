fafbdef89353d23e3e8fd0222b31687229cace71
Nathies <nathies1414@gmail.com>
2026-03-22 21:15:39 +0530

    routes defined and awaiting implementation

--- index.js
+++ index.js
@@ -8,31 +8,31 @@ const express = require("express");
 const PORT = 3000;
 
 const username = [{
-    id: 1,
+    id: "1",
     username: "nathies",
     password: "2345"
 }, {
-    id: 2,
+    id: "2",
     username: "kirthiga",
     password: "99898"
 }];
 
 const organization = [{
-    id: org1,
+    id: "org1",
     organizationName: "100xdevs cohort",
-    admin: 1,
-    members: [2]
+    admin: "1",
+    members: ["2"]
 }];
 
 const boards = [{
-    id: b1,
-    organizationId: org1,
+    id: "b1",
+    organizationId: "org1",
     title: "Frontend",
 }];
 
 const issues = [{
-    id: i1,
-    boardsId: b1,
+    id: "i1",
+    boardsId: "b1",
     title: "Create Dark mode"
 }];
 
@@ -40,4 +40,26 @@ const app = express();
 
 app.use(express.json());
 
-app.listen(PORT);
+
+// Post - CREATE
+signup
+signin
+createOrg
+addMemberToOrg
+createBoard
+creareIssue
+
+// get - READ
+boards
+issues
+members
+
+// put - UPDATE
+issues
+
+// delete - DELETE
+members
+
+app.listen(PORT, () => {
+    console.log(`The server is running in ${PORT}`);
+});
